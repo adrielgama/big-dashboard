98b899e5c2665df8c401a059a5ed62b0c073df16
Adriel Gama <adrielgama@gmail.com>
2024-02-20 22:28:59 -0300

    chore: skeleton adjustments

--- src/components/skeletons/cards.tsx
+++ src/components/skeletons/cards.tsx
@@ -1,14 +1,23 @@
 import React from 'react'
 
+import { useSidebarContext } from '@/context/SidebarContext'
+
 import { Skeleton } from '../ui/skeleton'
 
 export const Cards: React.FC = () => {
+  const { isOpen } = useSidebarContext()
   return (
     <div className="rounded-md bg-big-gray-200 p-6">
       <div className="space-y-2">
-        <Skeleton className="h-4 w-[150px] rounded-xl" />
-        <Skeleton className="h-4 w-[250px] rounded-xl" />
-        <Skeleton className="h-4 w-[250px] rounded-xl" />
+        <Skeleton
+          className={`h-4 w-[150px] ${isOpen ? 'lg:w-[100px]' : 'lg:w-[150px]'} rounded-xl xl:w-[150px]`}
+        />
+        <Skeleton
+          className={`h-4 w-[200px] ${isOpen ? 'lg:w-[150px]' : 'lg:w-[180px]'} rounded-xl xl:w-[200px]`}
+        />
+        <Skeleton
+          className={`h-4 w-[200px] ${isOpen ? 'lg:w-[150px]' : 'lg:w-[180px]'} rounded-xl xl:w-[200px]`}
+        />
       </div>
     </div>
   )
--- src/components/skeletons/shortNews.tsx
+++ src/components/skeletons/shortNews.tsx
@@ -7,10 +7,7 @@ export const ShortNews: React.FC = () => {
     <div className="max-h-16 rounded-md bg-big-gray-200 p-6">
       <div className="flex flex-row space-x-3">
         <Skeleton className="h-4 w-[100px] rounded-xl" />
-        <Skeleton className="h-4 w-[100px] rounded-xl" />
-        <Skeleton className="hidden h-4 w-[150px] rounded-xl lg:block" />
-        <Skeleton className="hidden h-4 w-[160px] rounded-xl lg:block" />
-        <Skeleton className="hidden h-4 w-[180px] rounded-xl lg:block" />
+        <Skeleton className="h-4 w-[150px] rounded-xl" />
       </div>
     </div>
   )
